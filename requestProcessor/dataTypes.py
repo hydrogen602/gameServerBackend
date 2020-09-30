68912fe7262e21a24bc829a5c65907d606f5164e
Jonathan Rotter <hydrogen31415@gmail.com>
2020-09-30 20:53:18 +0200

    split process func

--- requestProcessor/dataTypes.py
+++ requestProcessor/dataTypes.py
@@ -1,17 +1,57 @@
 from __future__ import annotations
 from typing import Optional, Tuple, TYPE_CHECKING
+from abc import ABC, abstractmethod
 if TYPE_CHECKING:
     from .game import AbstractGame
 
-class DummyPlayer:
-    def getGameID(self) -> Optional[str]: return NotImplemented
-    def setGameID(self, id: str): return NotImplemented
 
-class DummyDB:
-    def addPlayer(self) -> Tuple[str, DummyPlayer]: return NotImplemented
-    def getPlayer(self, id: str) -> Optional[DummyPlayer]: return NotImplemented
+class Player:
+    '''
+    The Player superclass. Subclass
+    this for additional functionality.
+    '''
 
-class DummyDB2:
+    def __init__(self):
+        self.__gameID: Optional[str] = None
+
+    def getGameID(self) -> Optional[str]:
+        '''
+        Get the id of the game the player is
+        currently a part of. returns None
+        if the player is not part of a game.
+        '''
+        return self.__gameID
+    
+    def setGameID(self, id: Optional[str]):
+        '''
+        
+        '''
+        self.__gameID = id
+
+
+class PlayerManager(ABC):
+
+    @abstractmethod
+    def addPlayer(self) -> Tuple[str, Player]:
+        return NotImplemented
+    
+    @abstractmethod
+    def getPlayer(self, id: str) -> Optional[Player]:
+        return NotImplemented
+
+
+class GameManager(ABC):
+
+    @abstractmethod
+    def getGame(self, id: str) -> Optional[AbstractGame]:
+        return NotImplemented
+
+
+class DummyDB(PlayerManager):
+    def addPlayer(self) -> Tuple[str, Player]: return NotImplemented
+    def getPlayer(self, id: str) -> Optional[Player]: return NotImplemented
+
+
+class DummyDB2(GameManager):
     def getGame(self, id: str) -> Optional[AbstractGame]: return NotImplemented
 
-Player = DummyPlayer
--- requestProcessor/requestProcess.py
+++ requestProcessor/requestProcess.py
@@ -1,28 +1,22 @@
 from __future__ import annotations
 from requestProcessor.errors import ActionError
 from . import interactions
-from .dataTypes import DummyDB, DummyDB2, Player
+from .dataTypes import DummyDB, DummyDB2, Player, PlayerManager, GameManager
 
 
-playerDatabase = DummyDB()
+# playerDatabase: PlayerManager = DummyDB()
 
-gameDatabase = DummyDB2()
+# gameDatabase: GameManager = DummyDB2()
 
-def process(r: interactions.UnprocessedClientRequest) -> interactions.Response:
+class RequestProcessor:
 
-    if r.playerID is None:
-        # new player id
-        playerID, playerData = playerDatabase.addPlayer()
-        r.setPlayerID(playerID)
-    else:
-        playerData = playerDatabase.getPlayer(r.playerID)
-        if playerData is None:
-            return interactions.ResponseFailure('Player not found in database')
+    def __init__(self, playerDatabase: PlayerManager, gameDatabase: GameManager) -> None:
+        self.playerDatabase = playerDatabase
+        self.gameDatabase = gameDatabase
+        super().__init__()
     
-    # now we get r.playerID and playerData
-
-    if isinstance(r, interactions.JoinGameClientRequest):
-        game = gameDatabase.getGame(r.gameID)
+    def __joinRequestProcess(self, playerData: Player, r: interactions.JoinGameClientRequest) -> interactions.Response:        
+        game = self.gameDatabase.getGame(r.gameID)
         # is game joinable? - not started & exists
         # if so, join
         if game is None:
@@ -37,13 +31,14 @@ def process(r: interactions.UnprocessedClientRequest) -> interactions.Response:
         else:
             raise TypeError(f'Expected type "{type(interactions.Response)}" but got type "{type(response)}"')
             #interactions.ResponseFailure('Unknown Error')
-    else:
+
+    def __regularRequestProcess(self, playerData: Player, r: interactions.UnprocessedClientRequest) -> interactions.Response:
         # standard request
         if r.request is None:
             return interactions.ResponseFailure('Empty request')
 
         gameID = playerData.getGameID()
-        game = gameDatabase.getGame(r.gameID)
+        game = self.gameDatabase.getGame(gameID)
         if game is None:
             return interactions.ResponseFailure('Game not found in database')
         
@@ -59,3 +54,21 @@ def process(r: interactions.UnprocessedClientRequest) -> interactions.Response:
             else:
                 raise TypeError(f'Expected type "{type(interactions.Response)}" but got type "{type(response)}"')
                 #interactions.ResponseFailure('Unknown Error')
+
+    def process(self, r: interactions.UnprocessedClientRequest) -> interactions.Response:
+
+        if r.playerID is None:
+            # new player id
+            playerID, playerData = self.playerDatabase.addPlayer()
+            r.setPlayerID(playerID)
+        else:
+            playerData = self.playerDatabase.getPlayer(r.playerID)
+            if playerData is None:
+                return interactions.ResponseFailure('Player not found in database')
+        
+        # now we get r.playerID and playerData
+
+        if isinstance(r, interactions.JoinGameClientRequest):
+            return self.__joinRequestProcess(playerData, r)
+        else:
+            return self.__regularRequestProcess(playerData, r)
